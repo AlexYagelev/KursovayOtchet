5a90441aa16be6e4cf6534b535d58fefd08c04ab
yagel <yagelev22@mail.ru>
2023-07-10 11:33:45 +0300

    Переделал прогу, под телефон локальный вер 9

--- app/src/main/java/com/programmerworld/aspose/ExcelDataSaver.java
+++ app/src/main/java/com/programmerworld/aspose/ExcelDataSaver.java
@@ -66,7 +66,7 @@ public class ExcelDataSaver {
 
 
         // Сохраняем файл Excel
-        String outputDirectory = "Внутренний общий накопитель/Download/"; // Место для сохранения файла
+        String outputDirectory = "/storage/emulated/0/Download"; // Место для сохранения файла
         String outputFile = outputDirectory + "/" + fileName;
         workbook.save(outputFile, SaveFormat.XLSX);
     }
